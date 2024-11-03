18757bc42a01997e117ac59e43981741a43b3718
Luiz Duarte <looduarte21@gmail.com>
2024-11-02 21:44:25 -0300

    feat: connect to mongodb

---
+++ src/database/mongoose.database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@taskmanagercluster.nydwj.mongodb.net/?retryWrites=true&w=majority&appName=TaskManagerCluster`,
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    );
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Failed to connect to MongoDB", error);
+  }
+};
+
+module.exports = connectToDatabase;
