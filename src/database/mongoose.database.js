b46913666096113a09f2526b691dc8fda59289ae
Luiz Duarte <looduarte21@gmail.com>
2024-11-07 20:23:11 -0300

    fix: database connection

--- src/database/mongoose.database.js
+++ src/database/mongoose.database.js
@@ -1,15 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 const connectToDatabase = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@taskmanagercluster.nydwj.mongodb.net/?retryWrites=true&w=majority&appName=TaskManagerCluster`,
-      { useNewUrlParser: true, useUnifiedTopology: true }
-    );
-    console.log("Connected to MongoDB");
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@taskmanagercluster.nydwj.mongodb.net/?retryWrites=true&w=majority&appName=TaskManagerCluster`
+    )
+    console.log("Connected to MongoDB")
   } catch (error) {
-    console.error("Failed to connect to MongoDB", error);
+    console.error("Failed to connect to MongoDB", error)
   }
-};
+}
 
-module.exports = connectToDatabase;
+module.exports = connectToDatabase
